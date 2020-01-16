567086de01220aca6ab71795b94982623daf7114
Илья <romachevif@gmail.com>
2020-01-16 20:09:25 +0300

    Пакман прямоходящий (Проверка веток)

--- Pac-Man.py
+++ Pac-Man.py
@@ -22,11 +22,16 @@ tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
 
-def load_image(name, colorkey=None):
+def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
-    image = pygame.image.load(fullname).convert()
-    if colorkey is not None:
-        if colorkey == -1:
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
     else:
